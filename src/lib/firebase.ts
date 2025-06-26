6ccf0aa2c3e54b5d3779a697fdaebba3b3b73b38
Kuswara Senjaya <senjayakuswara13@gmail.com>
2025-06-26 08:29:43 +0000

    saya berikan ke kamu ya ini:
    npm install firebase
    
    // Import the funct

---
+++ src/lib/firebase.ts
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp, getApps, getApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD9rX2jO_5bQ2ezK7sGv0QTMLcvy6aIhXE",
+  authDomain: "sekolah-ccec3.firebaseapp.com",
+  projectId: "sekolah-ccec3",
+  storageBucket: "sekolah-ccec3.firebasestorage.app",
+  messagingSenderId: "430648491716",
+  appId: "1:430648491716:web:1c3d389337adfd80d49391"
+};
+
+// Initialize Firebase
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+const db = getFirestore(app);
+
+export { app, db };
