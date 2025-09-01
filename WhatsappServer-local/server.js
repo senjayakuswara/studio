8d811df2f5e1901ad598f92705afe54bb3caad57
Kuswara Senjaya <senjayakuswara13@gmail.com>
2025-09-01 00:52:59 +0000

    hapus folder .wwebjs_auth sudah saya lakukan namun masih tetep

--- WhatsappServer-local/server.js
+++ WhatsappServer-local/server.js
@@ -1,7 +1,7 @@
 
 const express = require('express');
 const cors = require('cors');
-const { Client, LocalAuth, MessageMedia } = require('whatsapp-web.js');
+const { Client, LocalAuth, MessageMedia } = require('whatsapp-web.js-plus');
 const qrcode = require('qrcode-terminal');
 
 const app = express();
@@ -21,10 +21,6 @@ const client = new Client({
     puppeteer: {
         headless: true,
         args: ['--no-sandbox', '--disable-setuid-sandbox'],
-    },
-    webVersionCache: {
-      type: 'remote',
-      remotePath: 'https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2412.54.html',
     }
 });
 
