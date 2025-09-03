254089860f77b09c4854e6f44ce32610093295da
Kuswara Senjaya <senjayakuswara13@gmail.com>
2025-09-03 00:08:48 +0000

    =================================================================
      Mem

--- C:\Users\SMAPNA\Documents\WhatsappServer-local\start_ngrok.bat
+++ C:\Users\SMAPNA\Documents\WhatsappServer-local\start_ngrok.bat
@@ -1,9 +1,15 @@
+
 @echo off
+title Ngrok Tunnel for AbTrack
+
 echo =================================================================
 echo.
 echo   Memulai Ngrok Tunnel untuk Port 3000
+echo   Salin URL "Forwarding" (https://...) dan tempel di Pengaturan Aplikasi.
 echo   Biarkan jendela ini tetap terbuka!
 echo.
 echo =================================================================
-ngrok http 3000
-pause
+
+REM Menjalankan ngrok.exe dari folder saat ini
+.\ngrok.exe http 3000
+    
