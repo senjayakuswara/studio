24b37ea76ce6aeda00ab9246c7c098ad68501fc1
Kuswara Senjaya <senjayakuswara13@gmail.com>
2026-01-29 08:39:54 +0000

    oke namun nyatanya setelah saya ujicoba start.batnya seperti ini log nya

--- WhatsappServer-local/server.js
+++ WhatsappServer-local/server.js
@@ -106,6 +106,7 @@ function initializeWhatsApp() {
         },
         webVersionCache: {
           type: 'remote',
+          remotePath: 'https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2412.54.html'
         }
     });
 
